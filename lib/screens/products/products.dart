3cac226fd038e9ba196011e10e2157eb2c294832
Smith Bhojak <99sbhojak@gmail.com>
2020-09-28 17:30:25 +0530

    Added  image upload Functionality
    lib/screens/products/product.dart has 3 functions for selecting, uploading and clearing image and some containers below (also pubspec.yaml has latest required dependencies)

--- lib/screens/products/products.dart
+++ lib/screens/products/products.dart
@@ -1,23 +1,93 @@
 import 'package:flutter/material.dart';
-
+import 'package:firebase_storage/firebase_storage.dart'; // For File Upload To Firestore
+import 'package:hunar_seller/services/auth.dart';
+import 'package:image_picker/image_picker.dart'; // For Image Picker
+import 'package:path/path.dart' as Path;
+import 'dart:io';
 class Products extends StatefulWidget {
   @override
   _ProductsState createState() => _ProductsState();
 }
 
 class _ProductsState extends State<Products> {
+  final AuthService _auth = AuthService();
+  var uid;
+  File _image;
+  String _uploadedFileURL;
+  Future chooseFile() async {
+    // ignore: deprecated_member_use
+    await ImagePicker.pickImage(source: ImageSource.gallery).then((image) {
+      setState(() {
+        _image = image;
+      });
+    });
+  }
+
+  Future uploadFile() async {
+    uid=await _auth.getUser();
+    StorageReference storageReference = FirebaseStorage.instance
+        .ref()
+        //.child('test/$uid/${Path.basename(_image.path)}}');
+        .child('test/profile/$uid');
+    StorageUploadTask uploadTask = storageReference.putFile(_image);
+    await uploadTask.onComplete;
+    print('File Uploaded');
+    storageReference.getDownloadURL().then((fileURL) {
+      setState(() {
+        _uploadedFileURL = fileURL;
+      });
+    });
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         body: Container(
           padding: EdgeInsets.all(32),
-          child: Row(
+          child: Column(
 
-            mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
 
             children: <Widget>[
               Text("Products", textAlign: TextAlign.center,style: TextStyle(color: Colors.lightBlue[200], fontSize: 32),),
-
+              Text('Selected Image'),
+              _image != null
+                  ? Image.asset(
+                _image.path,
+                height: 150,
+              )
+                  : Container(height: 150),
+              _image == null
+                  ? RaisedButton(
+                child: Text('Choose File'),
+                onPressed: chooseFile,
+                color: Colors.cyan,
+              )
+                  : Container(),
+              _image != null
+                  ? RaisedButton(
+                child: Text('Upload File'),
+                onPressed: uploadFile,
+                color: Colors.cyan,
+              )
+                  : Container(),
+              _image != null
+                  ? RaisedButton(
+                child: Text('Clear Selection'),
+                onPressed: (){
+                  setState(() {
+                    _image = null;
+                  });
+                },
+              )
+                  : Container(),
+              Text('Uploaded Image'),
+              _uploadedFileURL != null
+                  ? Image.network(
+                _uploadedFileURL,
+                height: 150,
+              )
+                  : Container(),
             ],
           ),
         ),
