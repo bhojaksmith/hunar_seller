2fb0bd4945c810f41bd12ddfcb1c4b8714bb58d7
Smith Bhojak <99sbhojak@gmail.com>
2020-10-02 22:58:53 +0530

    Fixed products bug

--- lib/screens/products/add.dart
+++ lib/screens/products/add.dart
@@ -1,21 +1,24 @@
+
+
 import 'dart:io';
 
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:hunar_seller/authenticate/register2.dart';
+import 'package:hunar_seller/screens/products/add2.dart';
 import 'package:hunar_seller/services/auth.dart';
 import 'package:image_picker/image_picker.dart';
-class Register extends StatefulWidget {
+class AddProduct extends StatefulWidget {
   @override
-  _RegisterState createState() => _RegisterState();
+  _AddProductState createState() => _AddProductState();
 }
 
-class _RegisterState extends State<Register> {
+class _AddProductState extends State<AddProduct> {
   final AuthService _auth = AuthService();
   var uid;
   File _image;
   String _uploadedFileURL;
+
   Future chooseFile() async {
     // ignore: deprecated_member_use
     await ImagePicker.pickImage(source: ImageSource.gallery).then((image) {
@@ -27,7 +30,7 @@ class _RegisterState extends State<Register> {
   }
 
   Future uploadFile() async {
-    uid=await _auth.getUser();
+    uid = await _auth.getUser();
     StorageReference storageReference = FirebaseStorage.instance
         .ref()
     //.child('test/$uid/${Path.basename(_image.path)}}');
@@ -41,17 +44,21 @@ class _RegisterState extends State<Register> {
       });
     });
   }
-  void initState(){
+
+  void initState() {
     getUser();
     print('Getting user');
     super.initState();
   }
-  void getUser()async{
-    uid=await _auth.getUser();
+
+  void getUser() async {
+    uid = await _auth.getUser();
     print(uid);
   }
+
   final formKey = new GlobalKey<FormState>();
-  String sellerName,sellerDescription;
+  String productName, productPrice;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -74,13 +81,14 @@ class _RegisterState extends State<Register> {
               // ),
 
               Padding(
-                  padding: EdgeInsets.only(left: 5, right: 0,top:50,bottom: 50),
+                  padding: EdgeInsets.only(
+                      left: 5, right: 0, top: 50, bottom: 50),
                   child: Text(
-                    'Register Your Business ',
+                    'Add New Product ',
                     style: TextStyle(
                       fontSize: 32,
-                      color:Colors.black,
-                      fontWeight:FontWeight.w200,
+                      color: Colors.black,
+                      fontWeight: FontWeight.w200,
                     ),
                   )
               ),
@@ -90,32 +98,32 @@ class _RegisterState extends State<Register> {
                   child: TextFormField(
                     keyboardType: TextInputType.text,
 
-                    decoration:InputDecoration(
-                      labelText: 'Seller Name',
+                    decoration: InputDecoration(
+                      labelText: 'Product Name',
                       labelStyle: TextStyle(
                         color: Colors.grey,
                       ),
                     ),
                     onChanged: (val) {
                       setState(() {
-                        this.sellerName = val;
+                        this.productName = val;
                       });
                     },
                   )),
               Padding(
                   padding: EdgeInsets.only(left: 25.0, right: 25.0),
                   child: TextFormField(
-                    keyboardType: TextInputType.text,
+                    keyboardType: TextInputType.number,
 
-                    decoration:InputDecoration(
-                      labelText: 'Seller Description',
+                    decoration: InputDecoration(
+                      labelText: 'Product Price per Unit',
                       labelStyle: TextStyle(
                         color: Colors.grey,
                       ),
                     ),
                     onChanged: (val) {
                       setState(() {
-                        this.sellerDescription = val;
+                        this.productPrice = val;
                       });
                     },
                   )),
@@ -123,76 +131,81 @@ class _RegisterState extends State<Register> {
               SizedBox(height: 20,),
               Column(
 
-                  children: [
-                    // Text('Selected Image'),
-                    // _image != null
-                    //     ? Image.asset(
-                    //   _image.path,
-                    //   height: 150,
-                    // )
-                    //     : Container(height: 150),
-                    _image == null
-                        ? RaisedButton(
-                      child: Text('Upload Profile Picture'),
-                      onPressed: chooseFile,
-                      color: Colors.cyan,
-                    )
-                        : Container(),
-                    // _image != null
-                    //     ? RaisedButton(
-                    //   child: Text('Upload File'),
-                    //   onPressed: uploadFile,
-                    //   color: Colors.cyan,
-                    // )
-                    //     : Container(),
-                    _image != null
-                        ? RaisedButton(
-                      child: Text('Select Another?'),
-                      onPressed: (){
-                        chooseFile();
-                        // setState(() {
-                        //   _image = null;
-                        // });
-                      },
-                    )
-                        : Container(),
-                    Text('Uploaded Image'),
-                    _uploadedFileURL != null
-                        ? CircleAvatar(
-                      radius: 90,
-                          backgroundImage: NetworkImage(
+                children: [
+                  // Text('Selected Image'),
+                  // _image != null
+                  //     ? Image.asset(
+                  //   _image.path,
+                  //   height: 150,
+                  // )
+                  //     : Container(height: 150),
+                  _image == null
+                      ? RaisedButton(
+                    child: Text('Upload Product Picture'),
+                    onPressed: chooseFile,
+                    color: Colors.cyan,
+                  )
+                      : Container(),
+                  // _image != null
+                  //     ? RaisedButton(
+                  //   child: Text('Upload File'),
+                  //   onPressed: uploadFile,
+                  //   color: Colors.cyan,
+                  // )
+                  //     : Container(),
+                  _image != null
+                      ? RaisedButton(
+                    child: Text('Select Another?'),
+                    onPressed: () {
+                      chooseFile();
+                      // setState(() {
+                      //   _image = null;
+                      // });
+                    },
+                  )
+                      : Container(),
+                  Text('Uploaded Image'),
+                  _uploadedFileURL != null
+                      ? CircleAvatar(
+                    radius: 90,
+                    backgroundImage: NetworkImage(
                       _uploadedFileURL,
 
                     ),
-                        )
-                        : Container(),
-                  ],
-                ),
+                  )
+                      : Container(),
+                ],
+              ),
 
               Padding(
                   padding: EdgeInsets.only(left: 25.0, right: 25.0),
                   child: ClipRRect(
-                    borderRadius:BorderRadius.circular(10),
+                    borderRadius: BorderRadius.circular(10),
                     child: RaisedButton(
                         color: Colors.blue[200],
                         child: Center(
-                          child:Text('Next',style: TextStyle(color: Colors.white,letterSpacing: 4),
+                          child: Text('Next', style: TextStyle(
+                              color: Colors.white, letterSpacing: 4),
                           ),
                         ),
                         onPressed: () {
-                          Map<String,dynamic> testData = {
-                            'sellerName': sellerName ,
-                            'sellerDescription': sellerDescription ,
-                            'sellerImage':_uploadedFileURL,
-                          };
-                          // ignore: deprecated_member_use
-                          CollectionReference collectionReference = Firestore.instance.collection('testSeller');
+                          // Map<String, dynamic> testData = {
+                          //
+                          //   'userID': uid,
+                          //   'productName': productName,
+                          //   'productPrice': productPrice,
+                          //   'productURL': _uploadedFileURL,
+                          // };
+                          // // ignore: deprecated_member_use
+                          // CollectionReference collectionReference = Firestore
+                          //     .instance.collection('testSeller');
                           // ignore: deprecated_member_use
                           // only use set data for inserting the first time
-                          collectionReference.doc(uid).set(testData);
+                         // collectionReference.doc(uid).collection('products').doc().set(testData);
                           Navigator.push(
                             context,
-                            MaterialPageRoute(builder: (context) => Register2()),
+                            MaterialPageRoute(
+                                builder: (context) => AddProduct2(productName:productName,productPrice:productPrice,productURL:_uploadedFileURL)),
                           );
                         }
                     ),
